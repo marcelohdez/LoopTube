962e83fd623426d1e13df7b89beb211d0c90decd
Marcelo Hernandez <marcelohdez.inq@gmail.com>
2023-04-26 23:04:19 -0400

    make all LoopTube dialogs have multi-line messages
    Signed-off-by: Marcelo Hernandez <marcelohdez.inq@gmail.com>

--- src/main/java/me/marcelohdez/looptube/dialog/ErrorDialog.java
+++ src/main/java/me/marcelohdez/looptube/dialog/ErrorDialog.java
@@ -4,33 +4,16 @@ import javax.swing.*;
 import java.awt.*;
 
 public class ErrorDialog extends LoopTubeDialog {
-    private static final int WORDS_PER_LINE = 7;
-
     public ErrorDialog(Component summoner, String msg) {
-        super(summoner, "Oh no!", "Error descriptor:");
-
-        var msgArea = new JTextArea(makeParagraph(msg));
-        msgArea.setEditable(false);
+        super(summoner, "Oh no!", msg);
 
         var button = new JButton("Ok");
         button.addActionListener(e -> dispose());
 
-        add(msgArea);
         add(button, BorderLayout.PAGE_END);
         pack();
 
         setLocationRelativeTo(summoner);
         setVisible(true);
-    }
-
-    private String makeParagraph(String s) {
-        var words = s.split(" ");
-        if (words.length > WORDS_PER_LINE) {
-            for (int i = WORDS_PER_LINE; i < words.length; i += WORDS_PER_LINE) {
-                words[i] += '\n';
-            }
-        }
-
-        return String.join(" ", words);
     }
 }
--- src/main/java/me/marcelohdez/looptube/dialog/LoopTubeDialog.java
+++ src/main/java/me/marcelohdez/looptube/dialog/LoopTubeDialog.java
@@ -5,8 +5,10 @@ import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 abstract class LoopTubeDialog extends JDialog {
+    private static final int WORDS_PER_LINE = 7;
+
     protected final Component summoner;
-    protected final JLabel label = new JLabel();
+    protected final JTextArea label = new JTextArea();
 
     public LoopTubeDialog(Component summoner, String title, String text) {
         this.summoner = summoner;
@@ -15,9 +17,21 @@ abstract class LoopTubeDialog extends JDialog {
         setResizable(false);
         setModalityType(ModalityType.APPLICATION_MODAL);
 
-        label.setText(text);
-        label.setBorder(new EmptyBorder(6, 6, 6, 6));
+        label.setText(makeParagraph(text));
+        label.setBorder(new EmptyBorder(4, 4, 4, 4));
+        label.setEditable(false);
 
         add(label, BorderLayout.PAGE_START);
+    }
+
+    private static String makeParagraph(String s) {
+        var words = s.split(" ");
+        if (words.length > WORDS_PER_LINE) {
+            for (int i = WORDS_PER_LINE; i < words.length; i += WORDS_PER_LINE) {
+                words[i] += '\n';
+            }
+        }
+
+        return String.join(" ", words);
     }
 }
